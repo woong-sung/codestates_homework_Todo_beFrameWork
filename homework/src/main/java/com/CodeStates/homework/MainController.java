bb3267e77d3c30c803b2bbb43bbc92e3d32fe199
woong-sung <cjswo138@gmail.com>
2022-10-21 11:57:16 +0900

    ver 0.0.1

---
+++ homework/src/main/java/com/CodeStates/homework/MainController.java
@@ -0,0 +1,12 @@
+package com.CodeStates.homework;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/")
+    public String helloWorld(){
+        return "To-do Apllication by JSW";
+    }
+}
